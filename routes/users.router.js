11f15d0283473b0e7c2dc286e2227c1337cdd7a4
Kunta-M <mariia.kunta@gmail.com>
2021-10-11 15:13:54 +0300

    chngs

--- routes/users.router.js
+++ routes/users.router.js
@@ -3,30 +3,11 @@ const router = require('express').Router();
 const userController = require('../controllers/users.controller');
 const userMiddleware = require('../middlewares/user.middleware');
 
-router.get(
-    '/',
-    userController.getUsers);
+router.get('/', userController.getUsers);
+router.post('/', userMiddleware.isUserBodyValid, userMiddleware.checkUserByEmail, userController.createUser);
 
-router.post(
-    '/',
-    userMiddleware.isUserBodyValid,
-    userMiddleware.checkUserByEmail,
-    userController.createUser);
-
-router.get(
-    '/:user_id',
-    userMiddleware.checkUserById,
-    userController.getUserById);
-
-router.put(
-    '/:user_id',
-    userMiddleware.isUpdateBodyValid,
-    userMiddleware.checkUserById,
-    userController.updateUser);
-
-router.delete(
-    '/:user_id',
-    userMiddleware.checkUserById,
-    userController.deleteUser);
+router.get('/:user_id', userMiddleware.checkUserById, userController.getUserById);
+router.put('/:user_id', userMiddleware.isUpdateBodyValid, userMiddleware.checkUserById, userController.updateUser);
+router.delete('/:user_id', userMiddleware.checkUserById, userController.deleteUser);
 
 module.exports = router;
